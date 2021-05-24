c966ee98d83714d0ed6063330f33f883185a8713
Harsh Yadav <harshyadav9157@users.noreply.github.com>
2021-05-25 04:52:23 +0530

    commit

---
+++ registrationApp/College+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  College+CoreDataClass.swift
+//  registrationApp
+//
+//  Created by harsh yadav on 06/05/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(College)
+public class College: NSManagedObject {
+
+}
